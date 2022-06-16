fec9ab7242acf69b70e6bc4413c7baf01ed6c4c7
MATHI <76192299+mathisudar@users.noreply.github.com>
2022-06-16 23:16:44 +0530

    Create S1001_Greetings.js

---
+++ S1001_Greetings.js
@@ -0,0 +1,78 @@
+Bonjour - Hello
+Je m'appelle - I call myself
+Comment t'appelles tu? - How do you call yourself?
+Enchant'e(e) - Nice to meet you
+
+
+en -Ahn; on - Uhn; ou -Uhu; je - Jhu; tu - Chu; (e) - Feminine
+
+
+Bonsoir - Good Evening
+Bonne nuit - Good night
+Comment vas tu? - How are you?
+Je Vais[tres] bien - I am [very] well
+Et toi? - And you?
+Aurevoir - Good-bye 
+A bientot - See you soon
+Bravo - Congratulations
+
+
+oi -wa; ai-ah
+
+******************************************
+
+Bonjour!
+Bonjour!
+Comment t'appelles tu?
+Je m'appelle Mathi
+Comment t'appelles tu?
+Je m'appelle Clara.Enchantee.
+Enchantee.
+
+
+Good morning!
+Good morning!
+What's your name?
+My name is Mathi
+What's your name?
+My name is Clara.Nice to meet you.
+Nice to meet you.
+
+********************************************
+
+Bonsoir!
+Bonsoir!
+Comment vas tu?
+Je vais tres bien,et toi?
+Je vais tres bien.Merci!
+
+
+Good evening!
+Good evening!
+How are you doing?
+I'm very well and you?
+I am very well thank you!
+
+********************************************
+
+Bonjour!
+Bonjour!
+Je 'appelle Mathi.Comment t'appelles tu?
+Je m'appelle Clara.Enchantee.
+Enchantee.Comment vas tu?
+Je vais bien,et toi?
+Je vais bien.
+A bientot
+Aurevoir
+
+Good morning!
+Good morning!
+My name is Mathi. What's your name?
+My name is Clara.Nice to meet you.
+Nice to meet you. How are you?
+I'm doing well, and you?
+I'm doing well.
+See you soon
+Goodbye
+
+********************************************
